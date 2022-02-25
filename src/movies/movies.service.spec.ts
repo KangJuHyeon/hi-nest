c3a4a303b6e68cd93527807993fa30aa8b5d53a0
KangJuHyeon <alinos@naver.com>
2022-02-25 15:47:54 +0900

    #3.2 Testing getAll and getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -16,8 +17,34 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
-  // 첫 번째 테스트 작성
-  it('should be 4', () => {
-    expect(2 + 3).toEqual(5);
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      // getAll()이 배열을 리턴하는지 안 하는지 테스트한다.
+      // result가 배열 인스턴스인지 테스트하는 것이다.
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Move',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      // movie를 리턴하고, id는 1이어야 한다.
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID: 999 not found.');
+      }
+    });
   });
 });
