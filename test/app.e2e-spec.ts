be8bd3a149ea9e858c66efe092a6386a8b948e88
KangJuHyeon <alinos@naver.com>
2022-02-25 16:54:21 +0900

    #4.0 Testing movies (GET, POST, DELETE TESTING)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,29 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcom to my Movie API');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+
+    // 서버에 request해서 movies에 post할 때 이 정보를 보내면 201을 받는지 테스트하는거야
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+
+    // 404가 잘나오는 것을 확인
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
   });
 });
