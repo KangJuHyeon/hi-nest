ef9debea1f89b566e083434c90ecf3eeb7827931
KangJuHyeon <alinos@naver.com>
2022-02-25 17:12:24 +0900

    #4.1 Testing GET movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication(); // 어플리케이션을 생성하고 있다. 그래서 테스트할땐 pipe 구축도 다시 해줘야한다.
+    app.useGlobalPipes(
+      new ValidationPipe({
+        // 타입스크립트 자체에서 코드에 대한 유효성 검사를 해주는 매우 좋은 아이
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true, // 유저들이 보낸 것을 우리가 원하는 실제 타입으로 변환해준다.
+      }),
+    );
     await app.init();
   });
 
@@ -42,6 +50,17 @@ describe('AppController (e2e)', () => {
     // 404가 잘나오는 것을 확인
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+
+    describe('/movies/:id', () => {
+      it('GET 200', () => {
+        return request(app.getHttpServer()).get('/movies/1').expect(200);
+      });
+      it('GET 404', () => {
+        return request(app.getHttpServer()).get('/movies/999').expect(404);
+      });
+      it.todo('POST');
+      it.todo('PATCH');
     });
   });
 });
